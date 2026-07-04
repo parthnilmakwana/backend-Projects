56fb83f00e46c6d0bac3de59d8504d23f0b88132
parthnilmakwana <parthnilmakwana@gmail.com>
2026-07-04 20:07:10 +0530

    fix: remove unnecessary blank line in user routes

--- youtubeBackend/src/routes/user.routes.js
+++ youtubeBackend/src/routes/user.routes.js
@@ -17,7 +17,6 @@ router.route("/register").post(
 
     router.route("/login").post(loginUser);
     router.route("/logout").post(jwtVerify, logoutUser);
-    
     router.route("/refresh-token").post(refreshAccessToken);
 
 
